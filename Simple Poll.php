049157404d248c95f144351be83dec5881ce6480
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-18 13:34:52 +0200

    Create Simple Poll.php

---
+++ Simple Poll.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Simple Poll</title>
+		<style type="text/css">
+			.outer {
+				width: 300px;
+				height: 10px;
+				border: 1px solid #000;
+				background-color: #fff;
+			}
+			.inner {
+				height: 10px;
+				background-color: #690;
+			}
+		</style>
+	</head>
