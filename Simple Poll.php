3145784b35be6568934c1eb0bee9e8ac0a77a4e6
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-18 13:35:43 +0200

    database credentials

--- Simple Poll.php
+++ Simple Poll.php
@@ -22,3 +22,9 @@
 
 			// suppress error notices
 			error_reporting(E_ALL & ~E_NOTICE);
+            
+	          // database credentials
+			$db_user = 'root';
+			$db_password = '';
+			$db_host = 'localhost';
+			$db_name = 'dw'; 
