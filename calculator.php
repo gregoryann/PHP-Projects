5f7740ae99b87b2efccfc6d73c71c24c86f2a5c9
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-18 13:29:48 +0200

    Update calculator form

--- calculator.php
+++ calculator.php
@@ -52,3 +52,20 @@
 				}
 			
 			?>
+<!-- Calculator form -->
+		    <form method="post" action="calculator.php">
+		        <input name="number1" type="text" class="form-control" style="width: 150px; display: inline" />
+		        <select name="operation">
+		        	<option value="plus">Plus</option>
+		            <option value="minus">Minus</option>
+		            <option value="times">Times</option>
+		            <option value="divided by">Divided By</option>
+		        </select>
+		        <input name="number2" type="text" class="form-control" style="width: 150px; display: inline" />
+		        <input name="submit" type="submit" value="Calculate" class="btn btn-primary" />
+		    </form>
+	    
+		</div>
+	
+	</body>
+</html>
