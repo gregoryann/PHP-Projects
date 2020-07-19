db0bb7172af484eebff379847f741600d71c20a1
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-19 17:19:12 +0200

    Update db.php

--- PHP-PDO-Insert/db.php
+++ PHP-PDO-Insert/db.php
@@ -1,1 +1,28 @@
 
+<?php 
+
+class DB {
+    private $dbHost = "localhost";
+    private $dbUser = "root";
+    private $dbPassword = "";
+    private $dbName = "test";
+    private $conn;
+
+    public function __construct() {
+        try {
+            $dsn = "mysql:host=" . $this->dbHost . ";dbname=" . $this->dbName;
+            $this->conn = new PDO($dsn, $this->dbUser, $this->dbPassword);
+
+            // echo "Database connected";
+        } catch(PDOException $e) {
+            die("Connection failed");
+        }
+    }
+
+    public function insertData($name, $email) {
+        $sql = "INSERT INTO userdetails (name, email) VALUES (:name, :email)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute(['name' => $name, 'email' => $email]) or die(print_r($stmt->errorInfo(), true));;
+        echo "data inserted";
+    }
+}
